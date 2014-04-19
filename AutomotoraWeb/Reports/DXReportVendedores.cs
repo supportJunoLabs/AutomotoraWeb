6a49c2ca7947dd97fd4c42ea7f68f8fcaf017c31
Leandro Carrasco <lcarrasco.junolabs@gmail.com>
2014-04-19 02:50:25 -0300

    * Se modifico Reporte

--- AutomotoraWeb/Reports/DXReportVendedores.cs
+++ AutomotoraWeb/Reports/DXReportVendedores.cs
@@ -14,7 +14,25 @@ public class DXReportVendedores : DevExpress.XtraReports.UI.XtraReport {
     private TopMarginBand TopMargin;
     private DetailBand Detail;
     private XRLabel xrLabel1;
-    private System.Windows.Forms.BindingSource bindingSource1;
+    private XRTable xrTable1;
+    private XRTableRow xrTableRow1;
+    private XRTableCell xrTableCell1;
+    private XRTableCell xrTableCell2;
+    private XRTableCell xrTableCell3;
+    private XRTableCell xrTableCell4;
+    private AutomotoraWeb.DataSetAutomotoraWeb dataSetAutomotoraWeb1;
+    private AutomotoraWeb.DataSetAutomotoraWebTableAdapters.T_VendedoresTableAdapter t_VendedoresTableAdapter;
+    private XRTableCell xrTableCell9;
+    private GroupHeaderBand GroupHeader1;
+    private XRTable xrTable2;
+    private XRTableRow xrTableRow2;
+    private XRTableCell xrTableCell7;
+    private XRTableCell xrTableCell10;
+    private XRTableCell xrTableCell5;
+    private XRTableCell xrTableCell8;
+    private XRTableCell xrTableCell6;
+    private FormattingRule formattingRulePar;
+    private FormattingRule formattingRuleImpar;
     /// <summary>
     /// Required designer variable.
     /// </summary>
@@ -45,19 +63,40 @@ public class DXReportVendedores : DevExpress.XtraReports.UI.XtraReport {
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
             this.PageHeader = new DevExpress.XtraReports.UI.PageHeaderBand();
             this.PageFooter = new DevExpress.XtraReports.UI.PageFooterBand();
             this.BottomMargin = new DevExpress.XtraReports.UI.BottomMarginBand();
             this.TopMargin = new DevExpress.XtraReports.UI.TopMarginBand();
             this.Detail = new DevExpress.XtraReports.UI.DetailBand();
             this.xrLabel1 = new DevExpress.XtraReports.UI.XRLabel();
-            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.dataSetAutomotoraWeb1 = new AutomotoraWeb.DataSetAutomotoraWeb();
+            this.t_VendedoresTableAdapter = new AutomotoraWeb.DataSetAutomotoraWebTableAdapters.T_VendedoresTableAdapter();
+            this.xrTable1 = new DevExpress.XtraReports.UI.XRTable();
+            this.xrTableRow1 = new DevExpress.XtraReports.UI.XRTableRow();
+            this.xrTableCell1 = new DevExpress.XtraReports.UI.XRTableCell();
+            this.xrTableCell2 = new DevExpress.XtraReports.UI.XRTableCell();
+            this.xrTableCell3 = new DevExpress.XtraReports.UI.XRTableCell();
+            this.xrTableCell4 = new DevExpress.XtraReports.UI.XRTableCell();
+            this.xrTableCell9 = new DevExpress.XtraReports.UI.XRTableCell();
+            this.GroupHeader1 = new DevExpress.XtraReports.UI.GroupHeaderBand();
+            this.xrTable2 = new DevExpress.XtraReports.UI.XRTable();
+            this.xrTableRow2 = new DevExpress.XtraReports.UI.XRTableRow();
+            this.xrTableCell5 = new DevExpress.XtraReports.UI.XRTableCell();
+            this.xrTableCell6 = new DevExpress.XtraReports.UI.XRTableCell();
+            this.xrTableCell7 = new DevExpress.XtraReports.UI.XRTableCell();
+            this.xrTableCell8 = new DevExpress.XtraReports.UI.XRTableCell();
+            this.xrTableCell10 = new DevExpress.XtraReports.UI.XRTableCell();
+            this.formattingRulePar = new DevExpress.XtraReports.UI.FormattingRule();
+            this.formattingRuleImpar = new DevExpress.XtraReports.UI.FormattingRule();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetAutomotoraWeb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xrTable1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xrTable2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             // 
             // PageHeader
             // 
+            this.PageHeader.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
+            this.xrLabel1});
             this.PageHeader.Name = "PageHeader";
             // 
             // PageFooter
@@ -80,7 +119,10 @@ public class DXReportVendedores : DevExpress.XtraReports.UI.XtraReport {
             // Detail
             // 
             this.Detail.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
-            this.xrLabel1});
+            this.xrTable1});
+            this.Detail.FormattingRules.Add(this.formattingRulePar);
+            this.Detail.FormattingRules.Add(this.formattingRuleImpar);
+            this.Detail.HeightF = 35.50002F;
             this.Detail.Name = "Detail";
             this.Detail.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
             this.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
@@ -88,7 +130,7 @@ public class DXReportVendedores : DevExpress.XtraReports.UI.XtraReport {
             // xrLabel1
             // 
             this.xrLabel1.Font = new System.Drawing.Font("Arial", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.xrLabel1.LocationFloat = new DevExpress.Utils.PointFloat(179.1667F, 0F);
+            this.xrLabel1.LocationFloat = new DevExpress.Utils.PointFloat(162.5F, 25.62501F);
             this.xrLabel1.Name = "xrLabel1";
             this.xrLabel1.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F);
             this.xrLabel1.SizeF = new System.Drawing.SizeF(325F, 42.79167F);
@@ -97,6 +139,158 @@ public class DXReportVendedores : DevExpress.XtraReports.UI.XtraReport {
             this.xrLabel1.Text = "LISTADO DE VENDEDORES";
             this.xrLabel1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopCenter;
             // 
+            // dataSetAutomotoraWeb1
+            // 
+            this.dataSetAutomotoraWeb1.DataSetName = "DataSetAutomotoraWeb";
+            this.dataSetAutomotoraWeb1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // t_VendedoresTableAdapter
+            // 
+            this.t_VendedoresTableAdapter.ClearBeforeFill = true;
+            // 
+            // xrTable1
+            // 
+            this.xrTable1.FormattingRules.Add(this.formattingRulePar);
+            this.xrTable1.FormattingRules.Add(this.formattingRuleImpar);
+            this.xrTable1.LocationFloat = new DevExpress.Utils.PointFloat(0F, 10.00001F);
+            this.xrTable1.Name = "xrTable1";
+            this.xrTable1.Rows.AddRange(new DevExpress.XtraReports.UI.XRTableRow[] {
+            this.xrTableRow1});
+            this.xrTable1.SizeF = new System.Drawing.SizeF(650F, 25F);
+            // 
+            // xrTableRow1
+            // 
+            this.xrTableRow1.Cells.AddRange(new DevExpress.XtraReports.UI.XRTableCell[] {
+            this.xrTableCell3,
+            this.xrTableCell2,
+            this.xrTableCell1,
+            this.xrTableCell4,
+            this.xrTableCell9});
+            this.xrTableRow1.Name = "xrTableRow1";
+            this.xrTableRow1.Weight = 11.5D;
+            // 
+            // xrTableCell1
+            // 
+            this.xrTableCell1.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "T_Vendedores.direccion_vendedor")});
+            this.xrTableCell1.Name = "xrTableCell1";
+            this.xrTableCell1.Text = "xrTableCell1";
+            this.xrTableCell1.Weight = 0.5025640963040866D;
+            // 
+            // xrTableCell2
+            // 
+            this.xrTableCell2.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "T_Vendedores.fecha_ingreso_vendedor")});
+            this.xrTableCell2.Name = "xrTableCell2";
+            this.xrTableCell2.Text = "xrTableCell2";
+            this.xrTableCell2.Weight = 0.23333345853365389D;
+            // 
+            // xrTableCell3
+            // 
+            this.xrTableCell3.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "T_Vendedores.nombre_vendedor")});
+            this.xrTableCell3.Name = "xrTableCell3";
+            this.xrTableCell3.Text = "xrTableCell3";
+            this.xrTableCell3.Weight = 0.36923067533052883D;
+            // 
+            // xrTableCell4
+            // 
+            this.xrTableCell4.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "T_Vendedores.telefono_vendedor")});
+            this.xrTableCell4.Name = "xrTableCell4";
+            this.xrTableCell4.Text = "xrTableCell4";
+            this.xrTableCell4.Weight = 0.34230776742788471D;
+            // 
+            // xrTableCell9
+            // 
+            this.xrTableCell9.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "T_Vendedores.habilitado_vendedor")});
+            this.xrTableCell9.Name = "xrTableCell9";
+            this.xrTableCell9.Text = "xrTableCell9";
+            this.xrTableCell9.Weight = 0.15256400240384613D;
+            // 
+            // GroupHeader1
+            // 
+            this.GroupHeader1.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
+            this.xrTable2});
+            this.GroupHeader1.HeightF = 47.91667F;
+            this.GroupHeader1.Name = "GroupHeader1";
+            // 
+            // xrTable2
+            // 
+            this.xrTable2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold);
+            this.xrTable2.LocationFloat = new DevExpress.Utils.PointFloat(0F, 10.00001F);
+            this.xrTable2.Name = "xrTable2";
+            this.xrTable2.Rows.AddRange(new DevExpress.XtraReports.UI.XRTableRow[] {
+            this.xrTableRow2});
+            this.xrTable2.SizeF = new System.Drawing.SizeF(650F, 25F);
+            this.xrTable2.StylePriority.UseFont = false;
+            // 
+            // xrTableRow2
+            // 
+            this.xrTableRow2.Cells.AddRange(new DevExpress.XtraReports.UI.XRTableCell[] {
+            this.xrTableCell7,
+            this.xrTableCell10,
+            this.xrTableCell5,
+            this.xrTableCell8,
+            this.xrTableCell6});
+            this.xrTableRow2.Name = "xrTableRow2";
+            this.xrTableRow2.Weight = 11.5D;
+            // 
+            // xrTableCell5
+            // 
+            this.xrTableCell5.Name = "xrTableCell5";
+            this.xrTableCell5.Text = "Dirección";
+            this.xrTableCell5.Weight = 1.0470086122170472D;
+            // 
+            // xrTableCell6
+            // 
+            this.xrTableCell6.Name = "xrTableCell6";
+            this.xrTableCell6.Text = "Habilitado";
+            this.xrTableCell6.Weight = 0.31784167167467947D;
+            // 
+            // xrTableCell7
+            // 
+            this.xrTableCell7.Name = "xrTableCell7";
+            this.xrTableCell7.Text = "Nombre";
+            this.xrTableCell7.Weight = 0.76923061273036852D;
+            // 
+            // xrTableCell8
+            // 
+            this.xrTableCell8.Name = "xrTableCell8";
+            this.xrTableCell8.Text = "Telefono";
+            this.xrTableCell8.Weight = 0.71314114301632614D;
+            // 
+            // xrTableCell10
+            // 
+            this.xrTableCell10.Name = "xrTableCell10";
+            this.xrTableCell10.Text = "Fecha de Ingreso";
+            this.xrTableCell10.Weight = 0.48611129369491179D;
+            // 
+            // formattingRulePar
+            // 
+            this.formattingRulePar.Condition = "[DataSource.CurrentRowIndex] % 2 == 0";
+            // 
+            // 
+            // 
+            this.formattingRulePar.Formatting.BackColor = System.Drawing.Color.PapayaWhip;
+            this.formattingRulePar.Formatting.Borders = ((DevExpress.XtraPrinting.BorderSide)((((DevExpress.XtraPrinting.BorderSide.Left | DevExpress.XtraPrinting.BorderSide.Top) 
+            | DevExpress.XtraPrinting.BorderSide.Right) 
+            | DevExpress.XtraPrinting.BorderSide.Bottom)));
+            this.formattingRulePar.Name = "formattingRulePar";
+            // 
+            // formattingRuleImpar
+            // 
+            this.formattingRuleImpar.Condition = "[DataSource.CurrentRowIndex] % 2  !=  0";
+            // 
+            // 
+            // 
+            this.formattingRuleImpar.Formatting.BackColor = System.Drawing.Color.LemonChiffon;
+            this.formattingRuleImpar.Formatting.Borders = ((DevExpress.XtraPrinting.BorderSide)((((DevExpress.XtraPrinting.BorderSide.Left | DevExpress.XtraPrinting.BorderSide.Top) 
+            | DevExpress.XtraPrinting.BorderSide.Right) 
+            | DevExpress.XtraPrinting.BorderSide.Bottom)));
+            this.formattingRuleImpar.Name = "formattingRuleImpar";
+            // 
             // DXReportVendedores
             // 
             this.Bands.AddRange(new DevExpress.XtraReports.UI.Band[] {
@@ -104,11 +298,19 @@ public class DXReportVendedores : DevExpress.XtraReports.UI.XtraReport {
             this.TopMargin,
             this.BottomMargin,
             this.PageFooter,
-            this.PageHeader});
-            this.DataSource = this.bindingSource1;
+            this.PageHeader,
+            this.GroupHeader1});
+            this.DataAdapter = this.t_VendedoresTableAdapter;
+            this.DataMember = "T_Vendedores";
+            this.DataSource = this.dataSetAutomotoraWeb1;
+            this.FormattingRuleSheet.AddRange(new DevExpress.XtraReports.UI.FormattingRule[] {
+            this.formattingRulePar,
+            this.formattingRuleImpar});
             this.Margins = new System.Drawing.Printing.Margins(100, 100, 99, 100);
             this.Version = "13.2";
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetAutomotoraWeb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xrTable1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xrTable2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
 
     }
