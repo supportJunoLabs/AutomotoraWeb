111117564bfa4e71bcd35b94c0852c9c9bdff626
Mariel Feder <mfeder@netgate.com.uy>
2014-04-27 23:30:02 -0300

    ajustado mariel

--- AutomotoraWeb/Controllers/General/ErrorController.cs
+++ AutomotoraWeb/Controllers/General/ErrorController.cs
@@ -8,27 +8,9 @@ namespace AutomotoraWeb.Controllers.General
 {
     public class ErrorController : Controller
     {
-        public static string ERROR = "error";
-        public static string ERROR_403 = "error403";
-        public static string ERROR_404 = "error404";
-        public static string ERROR_500 = "error500";
-
-
-        [HttpGet]
-        public ActionResult Error403() {
-            HttpContext.Response.StatusCode = 403;
-            return View();
-        }
-
         [HttpGet]
         public ActionResult Error404() {
             HttpContext.Response.StatusCode = 404;
-            return View();
-        }
-
-        [HttpGet]
-        public ActionResult Error500() {
-            HttpContext.Response.StatusCode = 500;
             return View();
         }
 
