78d7b7ba38ea00c7a3a424faa3bed2c12b1dc39f
Mariel Feder <mfeder@netgate.com.uy>
2014-04-30 13:06:54 -0300

    pasados de configuracion a sistema

---
+++ AutomotoraWeb/Controllers/Sistema/EmpresaController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DLL_Backend;
+using AutomotoraWeb.Services;
+
+namespace AutomotoraWeb.Controllers.Sistema
+{
+    public class EmpresaController : SistemaController
+    {
+
+        public static string CONTROLLER = "empresa";
+
+        public ActionResult Edit() {
+            return getEmpresa();
+        }
+
+        private ActionResult getEmpresa() {
+            try {
+                Empresa emp = CompanyService.Empresa();
+                return View(emp);
+            } catch (UsuarioException exc) {
+                ViewBag.ErrorCode = exc.Codigo;
+                ViewBag.ErrorMessage = exc.Message;
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Empresa empresa) {
+            if (ModelState.IsValid) {
+                try {
+                    empresa.ModificarDatos();
+                    CompanyService.actualizarDatos(empresa);
+
+                    //TODO:  aca cambiar por la redireccion al index del ultimo modulo utilizado antes de acceder a esta opcion.
+                    return RedirectToAction(SistemaController.INDEX, SistemaController.BCONTROLLER);
+                } catch (UsuarioException exc) {
+                    ViewBag.ErrorCode = exc.Codigo;
+                    ViewBag.ErrorMessage = exc.Message;
+                    return View(empresa);
+                }
+            }
+
+            return View(empresa);
+        }
+
+     }
+}
---
+++ AutomotoraWeb/Controllers/Sistema/SistemaController.cs
@@ -0,0 +1,23 @@
+﻿using AutomotoraWeb.Controllers.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomotoraWeb.Controllers.Sistema {
+    public class SistemaController : BaseController {
+
+        public static string BCONTROLLER = "sistema";  
+        public static string INDEX = "index";  
+
+        public ActionResult Index() {
+            return View();
+        }
+
+        public override string getParentControllerName() {
+            return BCONTROLLER;
+        }
+
+    }
+}
