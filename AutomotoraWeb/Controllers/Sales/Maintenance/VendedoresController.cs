1a98d66b4e0648cedb6d3924855a49809b9733ef
Mariel Feder <mfeder@netgate.com.uy>
2014-05-03 01:47:14 -0300

    lc

--- AutomotoraWeb/Controllers/Sales/Maintenance/VendedoresController.cs
+++ AutomotoraWeb/Controllers/Sales/Maintenance/VendedoresController.cs
@@ -171,5 +171,23 @@ namespace AutomotoraWeb.Controllers.Sales.Maintenance {
 
         //-----------------------------------------------------------------------------------------------------
 
+        [HttpPost]
+        public JsonResult Upload() {
+            string fileName = "";
+            for (int i = 0; i < Request.Files.Count; i++) {
+                HttpPostedFileBase file = Request.Files[i]; //Uploaded file
+                //Use the following properties to get file's name, size and MIMEType
+                int fileSize = file.ContentLength;
+                fileName = file.FileName;
+                string mimeType = file.ContentType;
+                System.IO.Stream fileContent = file.InputStream;
+                //To save file, use SaveAs method
+                file.SaveAs(Server.MapPath("~/Content/Images/vendedores/") + fileName); //File will be saved in application root
+            }
+            return Json(new { fileName = fileName });
+        }
+
+        //-----------------------------------------------------------------------------------------------------
+
     }
 }
