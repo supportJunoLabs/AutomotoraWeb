072d37dbf5793c45a63289579280dbfd3b28737f
Mariel Feder <mfeder@netgate.com.uy>
2014-05-09 02:17:33 -0300

    ajuste detalle menor en controlador de cuentas bancarias

--- AutomotoraWeb/Controllers/Bank/Maintenance/CuentasBancariasController.cs
+++ AutomotoraWeb/Controllers/Bank/Maintenance/CuentasBancariasController.cs
@@ -111,6 +111,7 @@ namespace AutomotoraWeb.Controllers.Bank.Maintenance
 
             if (ModelState.IsValid) {
                 try {
+                    td.Moneda.Consultar();
                     td.Agregar();
                     return RedirectToAction(BaseController.SHOW);
                 } catch (UsuarioException exc) {
@@ -132,6 +133,7 @@ namespace AutomotoraWeb.Controllers.Bank.Maintenance
 
             if (ModelState.IsValid) {
                 try {
+                    td.Moneda.Consultar();
                     td.ModificarDatos();
                     return RedirectToAction(BaseController.SHOW);
                 } catch (UsuarioException exc) {
@@ -155,6 +157,7 @@ namespace AutomotoraWeb.Controllers.Bank.Maintenance
                 try {
                     string userName = (string)HttpContext.Session.Contents[SessionUtils.SESSION_USER_NAME];
                     string IP = HttpContext.Request.UserHostAddress;
+                    td.Moneda.Consultar();
                     td.Eliminar(userName, IP);
                     return RedirectToAction(BaseController.SHOW);
                 } catch (UsuarioException exc) {
