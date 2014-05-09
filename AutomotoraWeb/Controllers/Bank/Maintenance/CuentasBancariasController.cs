9b40c3726acf1ef8617d34e637264d8d1f8d194d
Mariel Feder <mfeder@netgate.com.uy>
2014-05-09 01:33:15 -0300

    NUEVO: Mantenimiento de cuentas bancarias. REQUIERE backend 20140509

---
+++ AutomotoraWeb/Controllers/Bank/Maintenance/CuentasBancariasController.cs
@@ -0,0 +1,171 @@
+﻿using AutomotoraWeb.Controllers.General;
+using AutomotoraWeb.Models;
+using AutomotoraWeb.Services;
+using AutomotoraWeb.Utils;
+using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DLL_Backend;
+using DevExpress.XtraReports.Parameters;
+using DevExpress.XtraReports.UI;
+using DevExpress.XtraPrinting;
+
+namespace AutomotoraWeb.Controllers.Bank.Maintenance
+{
+    public class CuentasBancariasController : BankController, IMaintenance {
+
+        public static string CONTROLLER = "CuentasBancarias";
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext) {
+            ViewBag.Monedas = Moneda.Monedas;
+            ViewBag.NombreEntidad = "Cuenta Bancaria";
+            ViewBag.NombreEntidades = "Cuentas Bancarias";
+            base.OnActionExecuting(filterContext);
+        }
+
+        public ActionResult Show([ModelBinder(typeof(DevExpressEditorsBinder))] CuentaBancaria td) {
+            return View(_listaElementos());
+        }
+
+        public ActionResult ListaGrilla() {
+            return PartialView("_listGrilla", _listaElementos());
+        }
+
+
+        //--------------------------------------    REPORT    ----------------------------------------------
+        public ActionResult Report() {
+            // Add a report to the view data. 
+            DXReportCuentasBancarias rep = new DXReportCuentasBancarias();
+            ViewData["Report"] = rep;
+            return View();
+        }
+
+        public ActionResult ReportPartial() {
+            DXReportCuentasBancarias rep = new DXReportCuentasBancarias();
+            rep.DataSource = _listaElementos();
+            ViewData["Report"] = rep;
+            return PartialView("_reportList");
+        }
+
+        public ActionResult ReportExport() {
+            DXReportCuentasBancarias rep = new DXReportCuentasBancarias();
+            //setParamsToReport(rep);
+            rep.DataSource = _listaElementos();
+            return DevExpress.Web.Mvc.DocumentViewerExtension.ExportTo(rep);
+        }
+
+       //--------------------------------------------------------------------------------------------------
+
+        public ActionResult Details(int id) {
+            ViewBag.SoloLectura = true;
+            return VistaElemento(id);
+        }
+
+        public ActionResult Create() {
+            return View();
+        }
+
+        public ActionResult Edit(int id) {
+            return VistaElemento(id);
+        }
+
+        public ActionResult Delete(int id) {
+            ViewBag.SoloLectura = true;
+            return VistaElemento(id);
+        }
+
+        //-----------------------------------------------------------------------------------------------------
+
+
+        private ActionResult VistaElemento(int id) {
+            try {
+                CuentaBancaria td = _obtenerElemento(id);
+                return View(td);
+            } catch (UsuarioException exc) {
+                ViewBag.ErrorCode = exc.Codigo;
+                ViewBag.ErrorMessage = exc.Message;
+                return View();
+            }
+        }
+
+        private CuentaBancaria _obtenerElemento(int id) {
+            CuentaBancaria td = new CuentaBancaria();
+            td.Codigo = id;
+            td.Consultar();
+            return td;
+        }
+
+        private List<CuentaBancaria> _listaElementos() {
+            return CuentaBancaria.CuentasBancarias();
+        }
+
+        //-----------------------------------------------------------------------------------------------------
+
+        [HttpPost]
+        public ActionResult Create(CuentaBancaria td) {
+
+            this.eliminarValidacionesIgnorables("Moneda", td.Moneda.IgnorablesDDL());
+
+            if (ModelState.IsValid) {
+                try {
+                    td.Agregar();
+                    return RedirectToAction(BaseController.SHOW);
+                } catch (UsuarioException exc) {
+                    ViewBag.ErrorCode = exc.Codigo;
+                    ViewBag.ErrorMessage = exc.Message;
+                    return View(td);
+                }
+            }
+
+            return View(td);
+        }
+
+        //-----------------------------------------------------------------------------------------------------
+
+        [HttpPost]
+        public ActionResult Edit(CuentaBancaria td) {
+
+            this.eliminarValidacionesIgnorables("Moneda", td.Moneda.IgnorablesDDL());
+
+            if (ModelState.IsValid) {
+                try {
+                    td.ModificarDatos();
+                    return RedirectToAction(BaseController.SHOW);
+                } catch (UsuarioException exc) {
+                    ViewBag.ErrorCode = exc.Codigo;
+                    ViewBag.ErrorMessage = exc.Message;
+                    return View(td);
+                }
+            }
+
+            return View(td);
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Delete(CuentaBancaria td) {
+            ViewBag.SoloLectura = true;
+            this.eliminarValidacionesIgnorables("Moneda", td.Moneda.IgnorablesDDL());
+
+            if (ModelState.IsValid) {
+                try {
+                    string userName = (string)HttpContext.Session.Contents[SessionUtils.SESSION_USER_NAME];
+                    string IP = HttpContext.Request.UserHostAddress;
+                    td.Eliminar(userName, IP);
+                    return RedirectToAction(BaseController.SHOW);
+                } catch (UsuarioException exc) {
+                    ViewBag.ErrorCode = exc.Codigo;
+                    ViewBag.ErrorMessage = exc.Message;
+                    return View(td);
+                }
+            }
+
+            return View(td);
+        }
+        //-----------------------------------------------------------------------------------------------------
+    }
+}
