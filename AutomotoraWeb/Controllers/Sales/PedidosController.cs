f8b955ff6c0b035f3576fd909200e274abeb052e
Mariel Feder <mfeder@netgate.com.uy>
2014-05-10 00:11:21 -0300

    nuevo: listado de pedidos

---
+++ AutomotoraWeb/Controllers/Sales/PedidosController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutomotoraWeb.Models;
+using DLL_Backend;
+
+namespace AutomotoraWeb.Controllers.Sales
+{
+    public class PedidosController : SalesController
+    {
+
+        public static string CONTROLLER = "Pedidos";
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext) {
+            ViewBag.NombreEntidad = "Pedido";
+            ViewBag.NombreEntidades = "Pedidos";
+            base.OnActionExecuting(filterContext);
+        }
+
+        public ActionResult Index(){
+            return View();
+        }
+
+        public ActionResult List() {
+            ListadoPedidosModel model = new ListadoPedidosModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult List( ListadoPedidosModel lf ) {
+            lf.Resultado = _listaElementos(lf.Filtro);
+            return View(lf);
+        }
+
+        public ActionResult ReportGrilla(ListadoPedidosModel model ) {
+            model.Resultado=_listaElementos(model.Filtro);
+            return PartialView("_reportGrilla", model);
+        }
+
+        private List<Pedido> _listaElementos(PedidoFiltro pf){
+            return Pedido.Pedidos(pf);
+        }
+    }
+}
