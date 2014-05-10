49e202e4ade769db240fd91b654f7da30b082420
Mariel Feder <mfeder@netgate.com.uy>
2014-05-10 00:11:11 -0300

    agregar posibilidad de configurar los botones de accion a la grilla dinamica de leandro.

---
+++ AutomotoraWeb/Models/BotonGrilla.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutomotoraWeb.Models {
+    public class BotonGrilla {
+        public string Accion { get; set; }
+        public string Controlador { get; set; }
+        public string Clase { get; set; }
+        public string Tooltip { get; set; }
+
+        //Constructor
+        public BotonGrilla(string accion, string controlador, string clase, string tooltip) {
+            Accion = accion;
+            Controlador = controlador;
+            Clase = clase;
+            Tooltip = tooltip;
+        }
+
+    }
+
+    
+}
