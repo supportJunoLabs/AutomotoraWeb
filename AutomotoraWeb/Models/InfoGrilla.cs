b1c2951f52d7b2bee0ea1aa7b2f71b7df91cd30a
Mariel Feder <mfeder@netgate.com.uy>
2014-05-22 20:21:59 -0300

    permitir incluir atributos de objetos asociados (ej: Cliente.Direccion)

--- AutomotoraWeb/Models/InfoGrilla.cs
+++ AutomotoraWeb/Models/InfoGrilla.cs
@@ -115,11 +115,25 @@ namespace AutomotoraWeb.Models {
         public string Hcontrolador { get; set; }
         public string HCampoParametro { get; set; }
 
+        //Se usan para marcar comportamiento especial en la grilla cuando son atributos que no se obtienen por metadata
+        //porque son atributos derivados.
+        //Se podria hacer por reflection recursivo, pero asi es mas facil, quizas se pueda mejorar mas adelante.
+        public bool EsEntero { get; set; }
+        public bool EsFecha { get; set; }
+        public bool EsBoolean { get; set; }
+        public bool EsImporte { get; set; }
+
+
+
         public ColumnaGrilla(){
             Alineacion=ALINEACIONES.DEFAULT;
             Ancho=0;
             LargoMax =0;
             Hipervinculo = false;
+            EsBoolean = false;
+            EsEntero = false;
+            EsFecha = false;
+            EsImporte = false;
         }
 
         public override bool Equals(object obj) {
--- AutomotoraWeb/Utils/GeneralUtils.cs
+++ AutomotoraWeb/Utils/GeneralUtils.cs
@@ -3,48 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Reflection;
+using AutomotoraWeb.Models;
 
 namespace AutomotoraWeb.Utils {
     public class GeneralUtils {
 
-        public static bool isBoolean(PropertyInfo prop) { 
-            if (prop.PropertyType.Name.Equals("Boolean")){
-                return true;
-            }
-            if (Nullable.GetUnderlyingType(prop.PropertyType) != null &&
-                Nullable.GetUnderlyingType(prop.PropertyType).Name.Equals("Boolean")) {
-                return true;
-            }
-            return false;
-        }
+        public static bool isBoolean(PropertyInfo prop, ColumnaGrilla cg) {
 
-        public static bool isImporte(PropertyInfo prop) {
-            if (prop.PropertyType.Name.Equals("Importe")) {
-                return true;
-            }
-            return false;
-        }
+            if (prop != null) {
 
-        public static bool isDateTime(PropertyInfo prop) { 
-            if (prop.PropertyType.Name.Equals("DateTime") ){
-                return true;
-            } 
-            if (Nullable.GetUnderlyingType(prop.PropertyType)!=null && 
-                Nullable.GetUnderlyingType(prop.PropertyType).Name.Equals("DateTime")) {
+                if (prop.PropertyType.Name.Equals("Boolean")) {
                     return true;
+                }
+                if (Nullable.GetUnderlyingType(prop.PropertyType) != null &&
+                    Nullable.GetUnderlyingType(prop.PropertyType).Name.Equals("Boolean")) {
+                    return true;
+                }
             }
-            return false;
+            return cg.EsBoolean;
         }
 
-        public static bool isInteger(PropertyInfo prop) {
-            if (prop.PropertyType.Name.Equals("Int32")) {
-                return true;
+        public static bool isImporte(PropertyInfo prop, ColumnaGrilla cg) {
+            if (prop != null) {
+                if (prop.PropertyType.Name.Equals("Importe")) {
+                    return true;
+                }
             }
-            if (Nullable.GetUnderlyingType(prop.PropertyType) != null &&
-                Nullable.GetUnderlyingType(prop.PropertyType).Name.Equals("Int32")) {
-                return true;
+            return cg.EsImporte;
+        }
+
+        public static bool isDateTime(PropertyInfo prop, ColumnaGrilla cg) {
+            if (prop != null) {
+                if (prop.PropertyType.Name.Equals("DateTime")) {
+                    return true;
+                }
+                if (Nullable.GetUnderlyingType(prop.PropertyType) != null &&
+                    Nullable.GetUnderlyingType(prop.PropertyType).Name.Equals("DateTime")) {
+                    return true;
+                }
             }
-            return false;
+            return cg.EsFecha;
+        }
+
+        public static bool isInteger(PropertyInfo prop, ColumnaGrilla cg) {
+            if (prop != null) {
+                if (prop.PropertyType.Name.Equals("Int32")) {
+                    return true;
+                }
+                if (Nullable.GetUnderlyingType(prop.PropertyType) != null &&
+                    Nullable.GetUnderlyingType(prop.PropertyType).Name.Equals("Int32")) {
+                    return true;
+                }
+            }
+            return cg.EsEntero;
         }
     }
 }
