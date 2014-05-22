50ff644dbc23de1544e14c4555eda26eddf34d25
Mariel Feder <mfeder@netgate.com.uy>
2014-05-22 20:22:40 -0300

    avances listado de ventas. Requiere version dll 22/5 y cambios en base de datos y scripts del menu

---
+++ AutomotoraWeb/Controllers/Sales/VentasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutomotoraWeb.Models;
+using DLL_Backend;
+using AutomotoraWeb.Utils;
+using DevExpress.XtraReports.Parameters;
+using DevExpress.XtraReports.UI;
+using DevExpress.XtraPrinting;
+
+namespace AutomotoraWeb.Controllers.Sales
+{
+    public class VentasController : SalesController
+    {
+
+        public static string CONTROLLER = "Ventas";
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext) {
+            base.OnActionExecuting(filterContext);
+            ViewBag.NombreEntidad = "Venta";
+            ViewBag.NombreEntidades = "Ventas";
+            
+        }
+
+        public ActionResult Index(){
+            return View();
+        }
+
+
+        //--------------------------METODOS PARA LISTADOS DE Ventas  -----------------------------
+
+        #region Listados
+
+        public ActionResult List() {
+
+            ListadoVentasModel model = new ListadoVentasModel();
+            string s = SessionUtils.generarIdVarSesion("ListadoVentas", Session[SessionUtils.SESSION_USER].ToString());
+            Session[s] = model;
+            model.idParametros = s;
+            ViewBag.SucursalesListado = Sucursal.Sucursales;
+            ViewBag.ClientesListado = Cliente.Clientes();
+            ViewBag.VendedoresListado = Vendedor.Vendedores(Vendedor.VEND_TIPO_LISTADO.TODOS);
+            ViewBag.TiposComubstiblesListado = TipoCombustible.TiposCombustible();
+            ViewData["idParametros"] = model.idParametros;
+            model.Resultado = _listaElementos(model);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult List(ListadoVentasModel model, string btnSubmit) {
+            Session[model.idParametros] = model; //filtros actualizados
+            ViewData["idParametros"] = model.idParametros;
+            ViewBag.SucursalesListado = Sucursal.Sucursales;
+            ViewBag.ClientesListado = Cliente.Clientes();
+            ViewBag.VendedoresListado = Vendedor.Vendedores(Vendedor.VEND_TIPO_LISTADO.TODOS);
+            ViewBag.TiposComubstiblesListado = TipoCombustible.TiposCombustible();
+            this.eliminarValidacionesIgnorables("Filtro.Sucursal", MetadataManager.IgnorablesDDL(model.Filtro.Sucursal));
+            this.eliminarValidacionesIgnorables("Filtro.Cliente", MetadataManager.IgnorablesDDL(model.Filtro.Cliente));
+            this.eliminarValidacionesIgnorables("Filtro.Vendedor", MetadataManager.IgnorablesDDL(model.Filtro.Vendedor));
+            if (ModelState.IsValid) {
+                if (btnSubmit == "Imprimir") {
+                    return this.Report(model);
+                }
+                model.Resultado = _listaElementos(model);
+            }
+            return View(model);
+        }
+
+        public ActionResult ReportGrilla(string idParametros) {
+            ListadoVentasModel model = (ListadoVentasModel)Session[idParametros];
+            model.Resultado = _listaElementos(model);
+            ViewData["idParametros"] = model.idParametros;
+            return PartialView("_reportGrilla", model);
+        }
+
+        private List<Venta> _listaElementos(ListadoVentasModel model) {
+            model.AcomodarFiltro();
+            return Venta.Ventas(model.Filtro);
+        }
+
+        #endregion
+        //---------- METODOS PARA REPORTES DE LISTADOS DE Ventas  -----------------------------
+
+        #region Reportes
+        public ActionResult Report(ListadoVentasModel model) {
+            return View("report", model);
+        }
+
+        public ActionResult ReportPartial(string idParametros) {
+            ListadoVentasModel model = null;
+            XtraReport rep = new DXListadoVentas();
+            model = (ListadoVentasModel)Session[idParametros];
+            rep.DataSource = _listaElementos(model);
+            setParamsToReport(rep, model); // lo hago despues porque listaElementos acomoda los filtros en model
+            Session[idParametros] = model;
+            ViewData["idParametros"] = idParametros;
+            ViewData["Report"] = rep;
+            return PartialView("_reportList");
+        }
+
+        private void setParamsToReport(XtraReport report, ListadoVentasModel model) {
+            Parameter paramSystemName = new Parameter();
+            paramSystemName.Name = "detalleFiltros";
+            paramSystemName.Type = typeof(string);
+            paramSystemName.Value = model.detallesFiltro();
+            paramSystemName.Description = "Detalle Filtros";
+            paramSystemName.Visible = false;
+            report.Parameters.Add(paramSystemName);
+        }
+
+        public ActionResult ReportExport(string idParametros) {
+            ListadoVentasModel model = null;
+            XtraReport rep = new DXListadoVentas();
+            model = (ListadoVentasModel)Session[idParametros];
+            setParamsToReport(rep, model);
+            rep.DataSource = _listaElementos(model);
+            return DevExpress.Web.Mvc.DocumentViewerExtension.ExportTo(rep);
+
+        }
+        #endregion
+
+
+
+    }
+}
---
+++ AutomotoraWeb/Scripts/AutomotoraWeb/VentasListado.js
@@ -0,0 +1,94 @@
+﻿//Configuracion inicial segun valores iniciales de los controles
+$(document).ready(function () {
+
+    var filtrarPeriodo = $("#cb_filtrarPeriodo").prop('checked')
+    _showHidePeriodo(filtrarPeriodo)
+
+    var filtrarSucursal = $("#cb_filtrarSucursal").prop('checked')
+    _showHideSucursal(filtrarSucursal);
+
+    var filtrarCliente = $("#cb_filtrarCliente").prop('checked')
+    _showHideCliente(filtrarCliente);
+
+    var filtrarVendedor = $("#cb_filtrarVendedores").prop('checked')
+    _showHideVendedor(filtrarVendedor);
+
+    var filtrarCombustible = $("#cb_filtrarCombustible").prop('checked')
+    _showHideCombustible(filtrarCombustible);
+
+
+});
+
+
+//Anexar las funciones de respuetsa al evento cambio en los controles
+
+$("#cb_filtrarPeriodo").change(function () {
+    var optionSelected = $(this).prop('checked')
+    _showHidePeriodo(optionSelected);
+});
+
+$("#cb_filtrarSucursal").change(function () {
+    var optionSelected = $(this).prop('checked')
+    _showHideSucursal(optionSelected);
+});
+
+$("#cb_filtrarCliente").change(function () {
+    var optionSelected = $(this).prop('checked')
+    _showHideCliente(optionSelected);
+});
+
+$("#cb_filtrarVendedores").change(function () {
+    var optionSelected = $(this).prop('checked')
+    _showHideVendedor(optionSelected);
+});
+
+$("#cb_filtrarCombustible").change(function () {
+    var optionSelected = $(this).prop('checked')
+    _showHideCombustible(optionSelected);
+});
+
+//Funciones que ocultan o muestran los filtro segun la opcion ingresada por el usuario
+
+function _showHidePeriodo(filtrar) {
+    if (filtrar == undefined || filtrar == false) {
+        $("#periodo").hide();
+        $("#fdesde").val(fechaTexto(sumarMeses( hoy(), -1) ) ) ;
+        $("#fhasta").val(fechaTexto( hoy() ) );
+    } else {
+        $("#periodo").show();
+    }
+}
+
+function _showHideSucursal(filtrar) {
+    if (filtrar == undefined || filtrar == false) {
+        $("#sucursales").hide();
+    } else {
+        $("#sucursales").show();
+    }
+}
+
+function _showHideCliente(filtrar) {
+    if (filtrar == undefined || filtrar == false) {
+        $("#clientes").hide();
+    } else {
+        $("#clientes").show();
+    }
+}
+
+function _showHideVendedor(filtrar) {
+    if (filtrar == undefined || filtrar == false) {
+        $("#vendedores").hide();
+    } else {
+        $("#vendedores").show();
+    }
+}
+
+function _showHideCombustible(filtrarCombustible) {
+    if (filtrarCombustible == undefined || filtrarCombustible == false) {
+        $("#combustibles").hide();
+    } else {
+        $("#combustibles").show();
+    }
+}
+
+
