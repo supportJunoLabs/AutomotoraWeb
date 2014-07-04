67277b2e476948aff9b1dcbf65626dec054113a4
Mariel Feder <mfeder@netgate.com.uy>
2014-07-04 02:01:23 -0300

    listado de transaccione completo (revisar luego doble clic) requiere DLL 03/jul

--- AutomotoraWeb/Controllers/Financing/TransaccionesController.cs
+++ AutomotoraWeb/Controllers/Financing/TransaccionesController.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DLL_Backend;
+using AutomotoraWeb.Models;
+using AutomotoraWeb.Utils;
+using DevExpress.XtraReports.Parameters;
+using DevExpress.XtraReports.UI;
+using DevExpress.XtraPrinting;
 
 namespace AutomotoraWeb.Controllers.Financing
 {
@@ -19,6 +24,11 @@ namespace AutomotoraWeb.Controllers.Financing
 
         protected override void OnActionExecuting(ActionExecutingContext filterContext) {
             base.OnActionExecuting(filterContext);
+            ViewBag.TiposRecibo = TipoRecibo.TiposRecibo();
+            ViewBag.Clientes = Cliente.Clientes();
+            ViewBag.Sucursales = Sucursal.Sucursales;
+            ViewBag.Usuarios = Usuario.Usuarios();
+            ViewBag.Financistas = Financista.FinancistasTodos;
         }
 
         public ActionResult ConsultaTransaccion(int id)
@@ -29,6 +39,86 @@ namespace AutomotoraWeb.Controllers.Financing
 
             return Redirect("/"+recibo.DestinoConsulta());
         }
+
+        #region ListadoTransacciones
+
+
+        //Se invoca desde la url del browser o desde el menu principal, o referencias externas. Devuelve la pagina completa
+        public ActionResult List() {
+            ListadoTransaccionesModel model = new ListadoTransaccionesModel();
+            string s = SessionUtils.generarIdVarSesion("ListadoTransacciones", Session[SessionUtils.SESSION_USER].ToString());
+            Session[s] = model;
+            model.idParametros = s;
+            ViewData["idParametros"] = model.idParametros;
+            model.obtenerListado();
+            return View(model);
+        }
+
+        [HttpPost]
+        //Se invoca desde el boton actualizar o imprimir.
+        public ActionResult List(ListadoTransaccionesModel model) {
+            Session[model.idParametros] = model; //filtros actualizados
+            ViewData["idParametros"] = model.idParametros;
+            //ViewBag.Financistas = Financista.Financistas(Financista.FIN_TIPO_LISTADO.TODOS);
+            this.eliminarValidacionesIgnorables("Filtro.Financista", MetadataManager.IgnorablesDDL(model.Filtro.Financista));
+            this.eliminarValidacionesIgnorables("Filtro.Sucursal", MetadataManager.IgnorablesDDL(model.Filtro.Sucursal));
+            this.eliminarValidacionesIgnorables("Filtro.Cliente", MetadataManager.IgnorablesDDL(model.Filtro.Cliente));
+            this.eliminarValidacionesIgnorables("Filtro.Usuario", MetadataManager.IgnorablesDDL(model.Filtro.Usuario));
+            this.eliminarValidacionesIgnorables("Filtro.TipoRecibo", MetadataManager.IgnorablesDDL(model.Filtro.TipoRecibo));
+            if (ModelState.IsValid) {
+                if (model.Accion == ListadoTransaccionesModel.ACCIONES.IMPRIMIR) {
+                    return this.ReportTransacciones(model);
+                }
+                model.obtenerListado();
+            }
+            return View(model);
+        }
+
+        //Se invoca desde paginacion, ordenacion etc, de grilla de cuotas. Devuelve la partial del tab de cuotas
+        public ActionResult ListGrillaTransacciones(string idParametros) {
+            ListadoTransaccionesModel model = (ListadoTransaccionesModel)Session[idParametros];
+            ViewData["idParametros"] = model.idParametros;
+            model.obtenerListado();
+            return PartialView("_listGrillaTransacciones", model.Resultado);
+        }
+
+        public ActionResult ReportTransacciones(ListadoTransaccionesModel model) {
+            return View("ReportTransacciones", model);
+        }
+
+        private XtraReport _generarReporteTransacciones(string idParametros) {
+            ListadoTransaccionesModel model = (ListadoTransaccionesModel)Session[idParametros];
+            model.obtenerListado();
+            XtraReport rep = new DXListadoTransacciones();
+            rep.DataSource = model.Resultado;
+            setParamsToReport(rep, model);
+            return rep;
+        }
+
+        public ActionResult ReportTransaccionesPartial(string idParametros) {
+            XtraReport rep = _generarReporteTransacciones(idParametros);
+            ViewData["idParametros"] = idParametros;
+            ViewData["Report"] = rep;
+            return PartialView("_reportTransacciones");
+        }
+
+        public ActionResult ReportTransaccionesExport(string idParametros) {
+            XtraReport rep = _generarReporteTransacciones(idParametros);
+            return DevExpress.Web.Mvc.DocumentViewerExtension.ExportTo(rep);
+        }
+
+        private void setParamsToReport(XtraReport report, ListadoTransaccionesModel model) {
+            Parameter param = new Parameter();
+            param.Name = "detalleFiltros";
+            param.Type = typeof(string);
+            param.Value = model.detallesFiltro();
+            param.Description = "Detalle Filtros";
+            param.Visible = false;
+            report.Parameters.Add(param);
+        }
+
+
+        #endregion
 
     }
 }
