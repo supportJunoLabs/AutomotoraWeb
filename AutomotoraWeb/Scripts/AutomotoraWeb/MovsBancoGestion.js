e46b2e1dead06683a20bdad29af986a81f6acf4b
Mariel Feder <mfeder@netgate.com.uy>
2014-07-07 19:31:24 -0300

    mover el javascript a archivo externo (mas prolijo)

---
+++ AutomotoraWeb/Scripts/AutomotoraWeb/MovsBancoGestion.js
@@ -0,0 +1,60 @@
+﻿$(document).ready(function () {
+
+    $(".boton-conciliar").live("click", function () {
+
+        var id = _getId($(this));
+
+        $.ajax({
+            url: '/Movimientos/Conciliar/',
+            //url: '@(Url.Action("Conciliar", "Movimientos"))',
+            type: 'POST',
+            async: true,
+            data: 'id=' + id,
+            success: function (data) {
+                if (data.Result == "OK") {
+                    grilla_movs.Refresh();
+                }
+                else {
+                    general_showAvisoPopup(data.ErrorMessage);
+                }
+            },
+            error: general_showErrorPopup,
+            beforeSend: showLoading,
+            complete: hideLoading
+        });
+    });
+
+    /* ============ */
+
+    $(".boton-desconciliar").live("click", function () {
+
+        var id = _getId($(this));
+
+        $.ajax({
+            url: '/Movimientos/Desconciliar/',
+            //url: '@(Url.Action("Desconciliar", "Movimientos"))',
+            type: 'POST',
+            async: true,
+            data: 'id=' + id,
+            success: function (data) {
+                if (data.Result == "OK") {
+                    grilla_movs.Refresh();
+                }
+                else {
+                    general_showAvisoPopup(data.ErrorMessage);
+                }
+            },
+            error: general_showErrorPopup,
+            beforeSend: showLoading,
+            complete: hideLoading
+        });
+    });
+
+});
+
+/* ================================================== */
+
+function _getId(elem) {
+    var str = elem.attr("id");
+    return str.split("_")[1];
+}
