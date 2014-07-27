52292687e142fc65078ba77f626c390c16edb2ec
Mariel Feder <mfeder@netgate.com.uy>
2014-07-27 00:07:44 -0300

    Consulta financiacion (nuevo)

---
+++ AutomotoraWeb/Models/ConsultaVentaModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DLL_Backend;
+
+namespace AutomotoraWeb.Models {
+    public class ConsultaVentaModel {
+        public Cliente Cliente { get; set; }
+        public Venta Venta { get; set; }
+
+    }
+}
---
+++ AutomotoraWeb/Scripts/AutomotoraWeb/ConsultaFinanciacion1.js
@@ -0,0 +1,50 @@
+﻿$(document).ready(function () {
+    var selectedCli = $("#ddlClientes").val();
+    if (selectedCli) {
+        $("#abtn_verCliente").prop("href", "/Clientes/details/" + selectedCli);
+    } else {
+        $("#abtn_verCliente").prop("href", "#");
+    }
+});
+
+$('#ddlClientes').change(function () {
+    /* Get the selected value of dropdownlist */
+    var selectedCli = $(this).val();
+    $("#abtn_verCliente").prop("href", "/Clientes/details/" + selectedCli);
+    $('#divVentasCliente').html('');
+    $('#divDetalleFinanciacion').html('');
+    var destino='/ConsultasFin/VentasFinCliente/';
+    $.ajax({
+        cache: false,
+        type: "GET",
+        url: destino,
+        data: { "idCliente": selectedCli },
+        success: function (data) {
+            $('#divVentasCliente').html(data);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            general_showErrorPopup(xhr, ajaxOptions, thrownError, destino);
+        },
+        beforeSend: showLoading,
+        complete: hideLoading
+    });
+});
+
+
+
+$('#btn_refresh').click(function () {
+    var selectedCli = $("#ddlClientes").val();
+    var selectedVenta = $("#codigoVenta").val();
+    if (selectedVenta) {
+        window.location = "/ConsultasFin/ConsultaFinanciacion/" + selectedVenta;
+        return;
+    }
+    if (selectedCli) {
+        window.location = "/ConsultasFin/ConsultaValesCliente/" + selectedCli;
+        return;
+    }
+
+});
+
+
+
