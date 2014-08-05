0b9ee4502af54ddb4ebb7268b349b103e722364d
Mariel Feder <mfeder@netgate.com.uy>
2014-08-05 00:29:18 -0300

    ayuda fechas

---
+++ AutomotoraWeb/Scripts/AutomotoraWeb/ayudaFechas.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $('.calendarAW').datepicker({
+        changeMonth: true,
+        dateFormat: "dd/mm/yy",
+        changeYear: true,
+        showAnim: 'slideDown',
+        yearRange: '1900:2100'
+    });
+});
