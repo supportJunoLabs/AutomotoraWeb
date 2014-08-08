5909f67f408296e00aa324429195889e73f3b459
Mariel Feder <mfeder@netgate.com.uy>
2014-08-07 21:35:32 -0300

    Funcionalidad de mensaje generico en pagina propia. Uso para confirmacion de cambio de clave.

--- AutomotoraWeb/Controllers/Sistema/SistemaController.cs
+++ AutomotoraWeb/Controllers/Sistema/SistemaController.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutomotoraWeb.Models;
 
 namespace AutomotoraWeb.Controllers.Sistema {
     public class SistemaController : BaseController {
 
-        public static string BCONTROLLER = "sistema";  
+        public static string BCONTROLLER = "sistema";
+        public static string CONTROLLER = "sistema";
         public static string INDEX = "index";  
 
         public ActionResult Index() {
@@ -21,6 +23,21 @@ namespace AutomotoraWeb.Controllers.Sistema {
 
         protected override void setearUltimoModulo() {
             //para este controller que no es ningun modulo, dejo el original.
+        }
+
+        public const int MSJ_CAMBIO_CLAVE_OK = 1;
+
+        public ActionResult Mensaje(int id) {
+            Mensaje msj = new Mensaje{Titulo="", Contenido=""};
+
+            switch (id) { 
+                case 1:
+                    msj.Titulo = "Cambio de Clave";
+                    msj.Contenido = "Ha cambiado su clave de acceso exitosamente";
+                    break;
+            }
+
+            return View(msj);
         }
     }
 }
