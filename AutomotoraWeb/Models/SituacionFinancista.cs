f9aec5a80147c148c24a0c3d91ee01eb7bc3e92b
Mariel Feder <mfeder@netgate.com.uy>
2014-08-09 03:20:18 -0300

    situacion financista: nuevo

---
+++ AutomotoraWeb/Models/SituacionFinancista.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DLL_Backend;
+
+
+namespace AutomotoraWeb.Models {
+    public class SituacionFinancista {
+        public Financista Financista { get; set; }
+        public List<FinancistaPagoCheque> Cheques { get; set; }
+        public List<FinancistaPagoEfectivo> Efectivo { get; set; }
+
+        public Importe TotalCheques { get; set; }
+        public Importe TotalEfectivo { get; set; }
+        public Importe TotalGeneral { get; set; }
+
+        public void generarSituacion(Financista fin) {
+            Financista = fin;
+            Cheques = fin.PagosChequesPendientes();
+            Efectivo = fin.PagosEfectivoPendientes();
+
+            TotalCheques = new Importe(Moneda.MonedaDefault, 0);
+            TotalEfectivo = new Importe(Moneda.MonedaDefault, 0);
+            foreach (FinancistaPagoCheque pf in Cheques) {
+                TotalCheques += pf.Cheque.Importe;
+            }
+            foreach (FinancistaPagoEfectivo pf in Efectivo) {
+                TotalEfectivo += (pf.ImporteOrig - pf.ImportePagoAnt);
+            }
+            TotalGeneral = TotalEfectivo + TotalCheques;
+        }
+    }
+    
+}
---
+++ AutomotoraWeb/Scripts/AutomotoraWeb/SitFinancista.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    var selectedID = $('#ddlFinancistas').val();
+    if (selectedID) {
+        $("#abtn_VerFinancista").prop("href", "/Financistas/details/" + selectedID);
+    }
+});
+
+$('#ddlFinancistas').change(function () {
+
+    /* Get the selected value of dropdownlist */
+    var selectedID = $(this).val();
+    $("#abtn_VerFinancista").prop("href", "/Financistas/details/" + selectedID);
+    $('#divSituacion').html('');
+    var destino = '/Financistas/ConsultaSituacionPartial/';
+    $.ajax({
+        cache: false,
+        type: "GET",
+        url: destino,
+        data: { "id": selectedID },
+        success: function (data) {
+            $('#divSituacion').html(data);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            general_showErrorPopup(xhr, ajaxOptions, thrownError, destino);
+        },
+        beforeSend: showLoading,
+        complete: hideLoading
+    });
+
+});
