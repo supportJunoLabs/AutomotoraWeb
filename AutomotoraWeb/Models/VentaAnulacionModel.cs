6c7a1240efcd610461ac6fe10679324145c2cb32
Mariel Feder <mfeder@netgate.com.uy>
2014-10-14 17:25:17 -0200

    anulacion de venta

---
+++ AutomotoraWeb/Models/VentaAnulacionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DLL_Backend;
+
+namespace AutomotoraWeb.Models {
+
+    //necesito este modelo para que los campos del model no se llamen igual a los campos de la venta, porque si no, se confunde en el postback.
+
+    public class VentaAnulacionModel {
+        public TRVentaAnulacion VentaDev { get; set; }
+    }
+}
---
+++ AutomotoraWeb/Scripts/AutomotoraWeb/VentaAnulacion.js
@@ -0,0 +1,59 @@
+﻿$(document).ready(function () {
+
+    inicializarModal("Confirmacion", "Confirma la ANULACION de la venta?", "Aceptar", "Cancelar");
+    $("#btnAnularVenta").live('click', function () { //antes de confirmar, traer lista de cheques dev seleccionados
+         gridChequesDevolver.GetSelectedFieldValues("Codigo", ObtenerCodigosConfirmarCallBack);
+     });
+     
+});
+
+//obtiene lista de cheques dev selecciondos y lo pone en  chequesDevolverIds y pide confirmacion para hacer el submit
+function ObtenerCodigosConfirmarCallBack(values) {
+    //alert("hola");
+    var selectedIDs;
+    selectedIDs = "";
+    for (var index = 0; index < values.length; index++) {
+        selectedIDs += values[index] + ",";
+    }
+    //alert(selectedIDs);
+    $("#chequesDevolverIds").val(selectedIDs);
+    $('#myModal').modal('show');
+}
+
+function callBackAceptar() {
+    $('form#formPrincipal').submit();
+}
+
+
+//Al seleccionar o des-selecconar un cheque devolver de la grilla, pasarlo a la lista de seleccionados. 
+function SelectionChangedChequesDev(s, e) {
+    //al hacer click en la grilla
+    s.GetSelectedFieldValues("ImporteTexto", GetSelectedFieldChequesDevCallback);
+}
+function GetSelectedFieldChequesDevCallback(values) {
+    //alert("xx");
+    //alert(values);
+    SelectedRows.BeginUpdate();
+    var tot = 0;
+    try {
+        SelectedRows.ClearItems();
+        for (var i = 0; i < values.length; i++) {
+            SelectedRows.AddItem(values[i]);
+            tot++;
+        }
+    } finally {
+        SelectedRows.EndUpdate();
+    }
+    $("#count").html(tot);
+}
+
+//al cargar la pagina, marcar como seleccionados los cheques que vienen en la lista (inicialmente todos)
+function ChequesDevInit() {
+    //alert($("#chequesDevolverIds").val());
+    if ($("#chequesDevolverIds").val()) {
+        var clavesch = $("#chequesDevolverIds").val().split(",");
+        gridChequesDevolver.SelectRowsByKey(clavesch);
+        //gridChequesDevolver.GetSelectedFieldValues("ImporteTexto", GetSelectedFieldChequesDevCallback);
+    }
+}
+
