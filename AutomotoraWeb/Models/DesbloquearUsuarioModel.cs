f8feb6285196a93d3ef7db08e5cf8e5fd5953923
Mariel Feder <mfeder@netgate.com.uy>
2014-11-03 00:38:01 -0200

    desbloquear usuario + performance devexpress

---
+++ AutomotoraWeb/Models/DesbloquearUsuarioModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace AutomotoraWeb.Models {
+    public class DesbloquearUsuarioModel {
+
+        [Required]
+        [DisplayName("Usuario Actual")]
+        [StringLength(30, ErrorMessage = "Usuario Actual: Largo maximo 30 caracteres")]
+        public string UsuarioActual { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [DisplayName("Contraseña Usuario Actual")]
+        [StringLength(30, ErrorMessage = "Clave Usuario Actual: Largo maximo 30 caracteres")]
+        public string ClaveUsuarioActual { get; set; }
+
+        [Required]
+        [DisplayName("Usuario a Desbloquear")]
+        [StringLength(30, ErrorMessage = "Usuario a Modificar: Largo maximo 30 caracteres")]
+        public string UsuarioReset { get; set; }
+
+    }
+}
